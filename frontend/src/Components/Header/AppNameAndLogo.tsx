44326c9644cd8fc0339306ab666970deca51f47e
unknown <aronpozsar@gmail.com>
2025-05-21 17:08:41 +0200

    added clickable logo that redirects to root

---
+++ frontend/src/Components/Header/AppNameAndLogo.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/react.svg";
+
+export default function AppNameAndLogo() {
+  return (
+    <div className="font-poppins flex items-center gap-2">
+      {/* Logo with Link */}
+      <Link to="/">
+        <div className="flex justify-center items-center">
+          <img src={Logo} alt="Logo" className="h-6 w-6" />
+        </div>
+      </Link>
+
+      {/* Container for the name */}
+      <div className="flex gap-1 items-center text-xl">
+        <span className="font-bold">Ide</span>
+        <span>Gas</span>
+      </div>
+    </div>
+  );
+}
--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import { Separator } from "../ui/separator";
 // import { ModeToggle } from "@/app/ModeToggle";
 import SearchBar from "./SearchBar";
-// import AppNameAndLogo from "./AppNameAndLogo";
+import AppNameAndLogo from "./AppNameAndLogo";
 
 export default function Navbar() {
   return (
     <div className="font-poppins fixed top-0 left-0 w-full p-6 flex justify-between items-center bg-white dark:bg-gray-900 shadow-md z-10">
       <div className="flex items-center gap-16">
-        {/* <AppNameAndLogo /> */}
+        <AppNameAndLogo />
         <SearchBar />
       </div>
       <div className="flex items-center gap-5">
