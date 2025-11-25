96971834676ce8e21098d924f68265c880789356
Andrija Turcic <andrija.turcic@gmail.com>
2025-11-25 18:45:31 +0100

    update router structure +

---
+++ backend/routes/posts.js
@@ -0,0 +1,45 @@
+import { Router } from 'express';
+import { body } from 'express-validator';
+import {
+  addComment,
+  createPost,
+  getAllPosts,
+  getPostById,
+  toggleLike,
+  getMyPosts,
+  getMemberPosts,
+  sharePost,
+  deletePost,
+} from '../controllers/posts.js';
+import { upload, checkToken } from '../common/middlewares.js';
+
+const router = Router();
+
+// Public routes
+router.get('/', getAllPosts);
+router.get('/:id', getPostById);
+router.get('/memberPosts/:username', getMemberPosts);
+
+// Protected routes
+router.post(
+  '/post',
+  checkToken,
+  upload.single('photo'),
+  body('caption').trim().escape().isLength({ min: 1, max: 300 }),
+  createPost
+);
+
+router.post(
+  '/:id/comments',
+  checkToken,
+  upload.none(),
+  body('text').trim().escape().isLength({ min: 1, max: 500 }),
+  addComment
+);
+
+router.get('/myPosts', checkToken, getMyPosts);
+router.post('/share', checkToken, sharePost);
+router.put('/:id/likes', checkToken, toggleLike);
+router.delete('/delete/:id', checkToken, deletePost);
+
+export default router;
